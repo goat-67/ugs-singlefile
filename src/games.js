88e35b128058d3d390088d6fcdc7fe3f5bb54993
goat <aiden.b.blood@gmail.com>
2026-03-18 18:00:01 -0600

    Update games.js

--- src/games.js
+++ src/games.js
@@ -4051,7 +4051,7 @@ const allGames = [
   },
   {
     "name": "clLastSwing",
-    "gameUrl": "https://cdn.jsdelivr.net/gh/goat-67/ugs-singlefile@8fe55286bd9ca0818e06fc42994a0acbb09734b4/UGS-Files/clLastSwing.html"
+    "gameUrl": "https://cdn.jsdelivr.net/gh/goat-67/ugs-singlefile@5427e5d115e0d951b16c55733a8d1fd7c8f027b9/UGS-Files/clLastSwing.html"
   },
   {
     "name": "cllegoindianajones2",
